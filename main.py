4d8df5876264285b480716ea5c31963ec308457a
Lobar Farxodova <qodirovjavohir2109@gmail.com>
2026-02-10 23:49:26 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,21 @@
+class Counter:
+    def __init__(self):
+        self.value = 0
+
+    def inc(self):
+        self.value += 1
+
+c = Counter()
+c.inc()
+c.inc()
+print(c.value)  # 2class Counter:
+    def __init__(self):
+        self.value = 0
+
+    def inc(self):
+        self.value += 1
+
+c = Counter()
+c.inc()
+c.inc()
+print(c.value)  # 2
